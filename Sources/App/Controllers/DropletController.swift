28ef6f41a21be3ef3afa7c2f46c0fc284c946c61
subash <thapa.subash345@gmail.com>
2019-12-24 10:09:20 -0800

    Use controllers

---
+++ Sources/App/Controllers/DropletController.swift
@@ -0,0 +1,59 @@
+import Foundation
+import Vapor
+
+struct DropletController: RouteCollection {
+    
+    func boot(router: Router) throws {
+        let dropletRoute = router.grouped("api","droplets")
+        dropletRoute.get(use: getAllHandler)
+        dropletRoute.get(Droplet.parameter, use: getHandler)
+        dropletRoute.post(use: createHandler)
+        dropletRoute.put(Droplet.parameter, use: updateHandler)
+        dropletRoute.delete(Droplet.parameter, use: deleteHandler)
+        dropletRoute.get("sorted", use: sortHandler)
+    }
+    
+    //Get: all
+    func getAllHandler(_ request: Request) throws -> Future<[Droplet]> {
+        return Droplet.query(on: request).all()
+    }
+    
+    //Get: specific droplet
+    func getHandler(_ request: Request) throws -> Future<Droplet> {
+        try request.parameters.next(Droplet.self)
+    }
+    
+    //Post: droplet
+    func createHandler(_ request: Request) throws -> Future<Droplet> {
+        try flatMap(to: Droplet.self,
+                    request.parameters.next(Droplet.self),
+                    request.content.decode(Droplet.self)) { (droplet, updatedDroplet)  in
+                        droplet.name = updatedDroplet.name
+                        return droplet.save(on: request)
+        }
+    }
+    
+    //Put: Update a droplet
+    func updateHandler(_ request: Request) throws -> Future<Droplet> {
+        try flatMap(to: Droplet.self,
+                    request.parameters.next(Droplet.self),
+                    request.content.decode(Droplet.self)) { (droplet, updatedDroplet)  in
+                        droplet.name = updatedDroplet.name
+                        return droplet.save(on: request)
+        }
+    }
+    
+    ///Delete: specific droplet
+    func deleteHandler(_ request: Request) throws -> Future<HTTPStatus> {
+        try request.parameters.next(Droplet.self).delete(on: request).transform(to: .noContent)
+    }
+    
+    //Sort
+    func sortHandler(_ request: Request) throws -> Future<[Droplet]> {
+        return Droplet.query(on: request)
+                   .sort(\.name, ._ascending)
+                   .all()
+    }
+    
+    
+}
